064f2b6f06e992b5d62bfedd3f68bf70d6b9ca49
Anas esseidi <esseidi.anas@gmail.com>
2018-01-23 17:28:43 +0000

    Youtube API
    Complete Consuming youtube API

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import BookList from '../containers/book_list'
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>Holla a todos</div>
+      <div>
+        <BookList />
+      </div>
     );
   }
 }
